560639ff14d7b345caa1cc1cbf5ca90cb46acaee
Le Dang <haidanglek7@gmail.com>
2026-07-27 16:59:55 +0700

    Connect frontend to backend API

---
+++ backend/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE schedule (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    day TEXT NOT NULL,
+    time TEXT NOT NULL,
+    title TEXT NOT NULL
+);
+
+INSERT INTO schedule(day,time,title)
+
+VALUES
+
+("Monday","08:00","Valorant");
+
+INSERT INTO schedule(day,time,title)
+
+VALUES
+
+("Tuesday","20:00","Liên Quân");
---
+++ backend/src/index.js
@@ -0,0 +1,44 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "https://hime.himehimzvtuber.workers.dev",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type",
+};
+
+export default {
+  async fetch(request, env) {
+
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        headers: corsHeaders,
+      });
+    }
+
+    const url = new URL(request.url);
+
+    if (url.pathname === "/api/schedule") {
+
+      const { results } = await env.hime_schedule
+        .prepare("SELECT * FROM schedule")
+        .all();
+
+      return Response.json(results, {
+        headers: corsHeaders,
+      });
+    }
+
+    return new Response("Not Found", {
+      status: 404,
+      headers: corsHeaders,
+    });
+  },
+};
